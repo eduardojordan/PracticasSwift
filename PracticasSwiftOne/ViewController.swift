f1f7284e4346d4e4e489cb99a54d5d85c4edd098
Eduardo Jordan <eduardojordan@hotmail.com>
2018-07-10 16:05:37 +0200

    cuadro-rojo

--- PracticasSwiftOne/ViewController.swift
+++ PracticasSwiftOne/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let v = UIView(frame:CGRect(x:100, y:100, width:50, height:50))
+        v.backgroundColor = .red // small red square
+        self.view.addSubview(v) // add it to main view
     }
 
     override func didReceiveMemoryWarning() {
