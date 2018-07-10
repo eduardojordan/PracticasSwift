614bef97183bcf29c300339caa64ba6e93a5e3c0
Eduardo Jordan <eduardojordan@hotmail.com>
2018-07-10 16:47:07 +0200

    manejo de colores y recuadros/desplazamientos de cuadros

--- PracticasSwiftOne/ViewController.swift
+++ PracticasSwiftOne/ViewController.swift
@@ -16,15 +16,27 @@ class ViewController: UIViewController {
 //        v.backgroundColor = .red // small red square
 //        self.view.addSubview(v) // add it to main view
  
-        let v1 = UIView(frame:CGRect(x:113, y:111, width:132, height:194))
-        v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
-        let v2 = UIView(frame:CGRect(x:41, y:56, width:132,height: 194))
-        v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
-        let v3 = UIView(frame:CGRect(x:43, y:197, width:160, height:230))
-       v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+//        let v1 = UIView(frame:CGRect(x:113, y:111, width:132, height:194))
+//        v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
+//        let v2 = UIView(frame:CGRect(x:41, y:56, width:132,height: 194))
+//        v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
+//        let v3 = UIView(frame:CGRect(x:43, y:197, width:160, height:230))
+//       v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+//        self.view.addSubview(v1)
+//        v1.addSubview(v2)
+//        self.view.addSubview(v3)
+        
+        let v1 = UIView(frame: CGRect (x:113,y:111,width: 132,height:194))
+        v1.backgroundColor = UIColor(red: 1, green:0.4, blue:1, alpha:1)
+        let v2 = UIView(frame: v1.bounds.insetBy(dx:10, dy:10))
+        v2.backgroundColor = UIColor(red:0.5,green:1, blue:0,alpha:1)
         self.view.addSubview(v1)
         v1.addSubview(v2)
-        self.view.addSubview(v3)
+        v1.bounds.origin.x += 10
+        v1.bounds.origin.y += 10
+
+
+        
     }
 
     override func didReceiveMemoryWarning() {
